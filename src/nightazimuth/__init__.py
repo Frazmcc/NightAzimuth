cfa43fd8e8818aa9cdcca65bbc2c35dfb50ff02a
Frazer McChlery <36603841+Frazmcc@users.noreply.github.com>
2026-09-11 00:04:49 +0100

    Set NightAzimuth v0.2.0-alpha.1

--- src/nightazimuth/__init__.py
+++ src/nightazimuth/__init__.py
@@ -2,6 +2,6 @@
 
 __all__ = ["__version__", "CREATOR", "COPYRIGHT"]
 
-__version__ = "0.1.0-alpha.1"
+__version__ = "0.2.0-alpha.1"
 CREATOR = "Logic Lurker"
 COPYRIGHT = "Created by Logic Lurker © 2026"
