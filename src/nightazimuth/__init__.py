3f816bf1b5c29b9b19de165933228151ee872346
Frazer McChlery <36603841+Frazmcc@users.noreply.github.com>
2026-09-10 20:13:46 +0100

    chore: add NightAzimuth package skeleton

---
+++ src/nightazimuth/__init__.py
@@ -0,0 +1,8 @@
+"""NightAzimuth package.
+
+Stage 2 contains project structure only. Functional satellite tracking,
+weather integration, and UI code will be added in later approved stages.
+"""
+
+__all__ = ["__version__"]
+__version__ = "0.1.0.dev0"
