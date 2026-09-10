3e15b9cd19f30a7b1ddac077d62eb277109d64dc
Frazer McChlery <36603841+Frazmcc@users.noreply.github.com>
2026-09-11 00:08:45 +0100

    Clean release package exports

--- src/nightazimuth/__init__.py
+++ src/nightazimuth/__init__.py
@@ -1,6 +1,6 @@
 """NightAzimuth satellite-tracking application."""
 
-__all__ = ["__version__", "CREATOR", "COPYRIGHT"]
+__all__ = ["COPYRIGHT", "CREATOR", "__version__"]
 
 __version__ = "0.2.0-alpha.1"
 CREATOR = "Logic Lurker"
