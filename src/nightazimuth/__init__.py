5042f1c44089cecde6b824cc3aaf71fc1baff519
Frazer McChlery <36603841+Frazmcc@users.noreply.github.com>
2026-09-13 20:24:08 +0100

    release: set application version to v0.5.0-beta.1

--- src/nightazimuth/__init__.py
+++ src/nightazimuth/__init__.py
@@ -2,6 +2,6 @@
 
 __all__ = ["COPYRIGHT", "CREATOR", "__version__"]
 
-__version__ = "0.4.0-beta.1"
+__version__ = "0.5.0-beta.1"
 CREATOR = "Logic Lurker"
 COPYRIGHT = "Created by Logic Lurker © 2026"
