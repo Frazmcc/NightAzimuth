3473a77cb519b92837cdb6d6b82733eefd5b48c3
Frazer McChlery <36603841+Frazmcc@users.noreply.github.com>
2026-09-26 02:44:30 +0100

    Move src/nightazimuth/api.py to nightazimuth.co.uk

--- src/nightazimuth/api.py
+++ src/nightazimuth/api.py
@@ -28,7 +28,7 @@ app = FastAPI(
 )
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://nightazimuth.bismo.me"],
+    allow_origins=["https://nightazimuth.co.uk"],
     allow_credentials=False,
     allow_methods=["GET"],
     allow_headers=["Accept"],
